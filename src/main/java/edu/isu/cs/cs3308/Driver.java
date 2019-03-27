e1a1d549aace28bd7d3f153ee6df051d015c05e8
tylebrad <tylebrad@Isu.edu>
2019-03-27 17:31:10 -0600

    Initial
    Added classes

---
+++ src/main/java/edu/isu/cs/cs3308/Driver.java
@@ -0,0 +1,10 @@
+// Bradley Tyler
+// Isaac Griffith
+// CS 3308
+// March - 27 - 2019
+
+// Driver class to run spell checker, using a set (of my own making).
+package edu.isu.cs.cs3308;
+
+public class Driver {
+}
